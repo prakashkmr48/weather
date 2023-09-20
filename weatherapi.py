1571ccf552fe63d864daed816ec839316b59bdc2
prakashkmr48 <prakashkmr48@gmail.com>
2023-09-20 23:28:01 +0530

    Update weatherapi.py

--- weatherapi.py
+++ weatherapi.py
@@ -17,7 +17,24 @@ def get_weather(api_key, city_name):
         return data
     else:
         return None
+# Markdown documentation for the Weather Data API
+weather_api_documentation = """
+# Weather Data API Documentation
 
+## Introduction
+
+The Weather Data API provides real-time weather information for a given location. It leverages the OpenWeatherMap API to fetch weather data based on the provided city name. This documentation outlines how to use the API, its endpoints, and the expected response.
+
+... (Include the entire documentation here)
+
+## Rate Limiting
+
+- OpenWeatherMap may impose rate limits on API usage for free or trial API keys. Refer to OpenWeatherMap's documentation for rate limit details.
+
+## Conclusion
+
+The Weather Data API allows you to access real-time weather data for a specified city. By following the guidelines provided in this documentation, you can integrate weather information into your applications or services with ease.
+"""
 
 def main():
     st.title("Weather App")
@@ -36,6 +53,10 @@ def main():
             st.write(f"Wind Speed: {weather_data['wind']['speed']} m/s")
         else:
             st.write("Unable to fetch weather data.")
+
+# Display the documentation when a button is clicked
+    if st.button("Show API Documentation"):
+        st.markdown(weather_api_documentation)
 
 
 if __name__ == "__main__":
